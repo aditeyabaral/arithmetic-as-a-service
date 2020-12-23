6791895c9bb19f34edfdc6a890856559ac563560
Aditeya Baral <aditeya.baral@gmail.com>
2020-12-23 23:36:01 +0530

    add basic functions

--- aaas.py
+++ aaas.py
@@ -1,23 +1,55 @@
 from flask import Flask, request
-from flask_limiter import Limiter
-from flask_limiter.util import get_ipaddr
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app, support_credentials=True)
 
-@app.route("/<path:vargs>", methods=["GET"])
-@cross_origin(support_credentials=True)
+
+@app.route("/add/<path:vargs>", methods=["GET"])
 def add(vargs):
-    result = 0
     try:
-        # Convert to integers and check bound constraints
-        for arg in vargs.split("/"):
-            if '1' <= arg <= '9':   # Check value constraints
-                result += int(arg)  # Find sum
+        numbers = list(map(float, vargs.split("/")))
+        result = numbers[0]
+        for num in numbers[1:]:
+            result += num
     except:
         return "Bad request", 400
     return str(result), 200
+
+
+@app.route("/sub/<path:vargs>", methods=["GET"])
+def sub(vargs):
+    try:
+        numbers = list(map(float, vargs.split("/")))
+        result = numbers[0]
+        for num in numbers[1:]:
+            result -= num
+    except:
+        return "Bad request", 400
+    return str(result), 200
+
+
+@app.route("/mul/<path:vargs>", methods=["GET"])
+def mul(vargs):
+    try:
+        numbers = list(map(float, vargs.split("/")))
+        result = numbers[0]
+        for num in numbers[1:]:
+            result *= num
+    except:
+        return "Bad request", 400
+    return str(result), 200
+
+
+@app.route("/div/<path:vargs>", methods=["GET"])
+def div(vargs):
+    try:
+        numbers = list(map(float, vargs.split("/")))
+        result = numbers[0]
+        for num in numbers[1:]:
+            result /= num
+    except:
+        return "Bad request", 400
+    return str(result), 200
+
 
 if __name__ == "__main__":
     app.run()
