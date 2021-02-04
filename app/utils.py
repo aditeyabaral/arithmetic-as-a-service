9b480699736b94e1d26cb41284e0bce89f8e1871
Aditeya Baral <aditeya.baral@gmail.com>
2021-02-04 21:06:51 +0530

    test deploy

--- app/utils.py
+++ app/utils.py
@@ -16,7 +16,7 @@ def getIPAddress():
 
 
 def getMACAddress():
-    mac = (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0, 48, 8)][::-1]))mac = (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0, 8*6, 8)][::-1]))
+    mac = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0, 48, 8)][::-1])
     mac = mac.upper()
     return mac
 
